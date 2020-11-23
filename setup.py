9fae4c012614b1fcd3907e66abf09d663b4114e1
Pbarbecho <pablo.barbecho@upc.edu>
2020-11-23 12:56:26 +0100

    python3.6

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author_email='pablo.barbecho@upc.edu',
     keywords='SUMO Outputs',
     packages=find_packages(),
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     install_requires=['matplotlib', 'numpy ', 'pandas', 'seaborn', 'argparse', 'Tk'],
     entry_points={
         'console_scripts': [
