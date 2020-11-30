5874455b2d1aa28125597b96956a9b64c00fbf08
Pbarbecho <pablo.barbecho@upc.edu>
2020-11-30 23:21:21 +0100

    Plot real vs predicted

--- sumo_statistics.py
+++ sumo_statistics.py
@@ -19,11 +19,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from xml.dom import minidom
 import seaborn as sns
-from sklearn.preprocessing import scale
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
-from utils import plot_predict_vs_real, xml2csv, lanes_counter_taz_locations, avrg_speed_and_geo_positions, veh_trip_info, remove_outage_points, encode_data, remove_features, add_features, feature_importance
+from utils import SUMO_preprocess, preprocess, scale_df, plot_predict_vs_real, feature_importance
 from keras.callbacks import EarlyStopping
 from sklearn.metrics import mean_squared_error
  
@@ -66,76 +65,30 @@ def sim_files_search(dir):
 def main(args=None):
     # get command line options
     options = get_options(args)
-    
-    """
-    # Process SUMO output files to build the dataset
-    sumo_dic = xml2csv(options)                                                                   # Get sumo output files into a dictionary of dataframes
-    taz_locations_edgenum_df = lanes_counter_taz_locations(sumo_dic['vehroute'])                  # Count edges on route from vehroute file and get from/to TAZ locations
-    veh_speed_positions_df = avrg_speed_and_geo_positions(sumo_dic['fcd'])                        # Get average speed and initial/end positions (x,y)
-    tripinfo_df = veh_trip_info(sumo_dic['tripinfo']) 
-    
-    # find/convert sumo simulation .rou/taz files
-    #rou, taz = sim_files_search(options.simfiles)
-    
-    # merge dataframes
-    data = taz_locations_edgenum_df.merge(veh_speed_positions_df,on='ID').merge(tripinfo_df,on='ID')
-    data.to_csv(os.path.join(options.sumofiles,'../parsed', 'data.csv'), index=False, header=True)
-    print('Parsed --> data.csv')
-    """
-    
-    # Just read not process sumo files
-    data = pd.read_csv(os.path.join(options.sumofiles,'../parsed', 'data.csv'))
-    data.drop(columns='Unnamed: 0', inplace=True)
-    #################################################################################
-    
-    # Preprocess data
-    data = remove_outage_points(data) 
-    data = encode_data(data)   
-    data = remove_features(data)                                                                  # remove unused features
-    data = add_features(data)                                                                     # add new problem features / based on what we know of the problem / we can add 15min restrictions here
-    
-    # Save preprocess data
-    data.to_csv(os.path.join(options.sumofiles,'../parsed', 'preprocess_data.csv'), index=False, header=True)
-    
-    # Scale preprocess values
-    explored_variable = 'tripinfo_duration'     #************************************************************
-    pre_scaled = data.copy()
-    df_scaled = pre_scaled.drop([explored_variable], axis=1)                                    # before scaling we should remove the output
-    df_scaled = scale(df_scaled)
-
-    # concat scaled and output ('trip_duration')
-    col = data.columns.tolist()
-    col.remove(explored_variable)
-    
-    df_scaled = pd.DataFrame(df_scaled, columns=col, index=data.index)
-    df_scaled = pd.concat([df_scaled, data[explored_variable]], axis=1)
-    
-    # Save scaled data
-    df_scaled.to_csv(os.path.join(options.sumofiles,'../parsed','scaled_data.csv'), index=False, header=True)            
- 
   
-    # # Split the dataframe into a training and testing set
-    df = df_scaled.copy()
+    # SUMO build dataframe Time comsuming process. Once generated comment #
+    data = SUMO_preprocess(options)
     
-    X = df.loc[:, df.columns != explored_variable] 
-    y = df[explored_variable]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-
-    X.to_csv(os.path.join(options.sumofiles,'../parsed','x.csv'), index=False, header=True)        
-    y.to_csv(os.path.join(options.sumofiles,'../parsed','y.csv'), index=False, header=True)        
-    X_train.to_csv(os.path.join(options.sumofiles,'../parsed','X_train.csv'), index=False, header=True)
-    y_train.to_csv(os.path.join(options.sumofiles,'../parsed','Y_train.csv'), index=False, header=True)
-
-    X_test.to_csv(os.path.join(options.sumofiles,'../parsed','X_test.csv'),  header=True)
-    y_test.to_csv(os.path.join(options.sumofiles,'../parsed','Y_test.csv'),  header=True)
-
+    # Read SUMO database 
+    data = pd.read_csv(os.path.join(options.sumofiles,'../parsed', 'data.csv'))
+    #data.drop(columns='Unnamed: 0', inplace=True)
+        
+    # Preprocess data
+    data = preprocess(data)                                    
+    
+    # Scale data
+    predicted_variable = 'tripinfo_duration'
+    df = scale_df(data, predicted_variable)
+   
+    # Split the dataframe into a training and testing set
+    X = df.loc[:, df.columns != predicted_variable] 
+    y = df[predicted_variable]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)        
+    
     # asses feature importance with random forest
     #feature_importance(df, X_train, y_train)
         
-        
     # Build neural network in Keras
-  
     model=Sequential()
     activation_f = 'relu'             # 'relu'
     model.add(Dense(128, activation= activation_f, input_dim=X_train.shape[1]))
@@ -150,7 +103,6 @@ def main(args=None):
     # overfitting
     #early_stopping = EarlyStopping(monitor='val_loss', patience=3)
     #model.fit(X_train, y_train, epochs=100,validation_split=0.2,callbacks=[early_stopping])
-    
     
     # Test model
     train_pred = model.predict(X_train)
@@ -169,6 +121,5 @@ def main(args=None):
     plot_predict_vs_real(test_pred, y_test, 100)
     
     
-
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -10,8 +10,10 @@ import pandas as pd
 import sys, os
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import scale
 import matplotlib.pyplot as plt
 
+parsed_dir = '/root/Documents/SUMO_SEM/CATALUNYA/parsed/'
 
 # import sumo tool xmltocsv
 if 'SUMO_HOME' in os.environ:
@@ -21,135 +23,171 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
-parsed_dir = '/root/Documents/SUMO_SEM/CATALUNYA/parsed/'
 
 
-def xml2csv(options): 
-    # convert xml to csv
-    files_list = os.listdir(options.sumofiles)
-    if files_list:
-        print('\nGenerating csv files .........')
-        converted_files_dic = {}
-        for f in files_list:
-            # output directory
-            output = os.path.join(options.sumofiles,'..','xmltocsv', f'{f.strip(".xml")}.csv')
-            # SUMO tool xml into csv
-            sumo_tool = os.path.join(tools, 'xml', 'xml2csv.py')
-            # Run sumo tool with sumo output file as input
-            cmd = 'python {} {} -s , -o {}'.format(sumo_tool, os.path.join(options.sumofiles,f), output)
-            os.system(cmd)
-            print(f'{f} -->  {f.strip(".xml")}.csv')
-            converted_files_dic[f.strip('.xml')] = pd.read_csv(output)  
-        return converted_files_dic
-    else:
-        sys.exit(f"Empty or missing output data files: {options.sumofiles}")
-
-
-
-def veh_trip_info(df):
-    # filter know features
-    df = df.filter(items=['tripinfo_duration', 
-                          'tripinfo_routeLength', 
-                          'tripinfo_timeLoss', 
-                          'tripinfo_waitingCount', 
-                          'tripinfo_waitingTime', 
-                          'tripinfo_arrivalLane', 
-                          'tripinfo_departLane',
-                          'tripinfo_id']).rename(columns={'tripinfo_id':'ID'})
-    return save_file(df, 'tripinfo_df')
-																	      
-    
 def save_file(df, name):
-    df.sort_values(by=['ID'], inplace=True)
+    if 'ID' in df.keys():
+        df.sort_values(by=['ID'], inplace=True)
     df.to_csv(os.path.join(parsed_dir, f'{name}.csv'), index=False, header=True)
     return df
     
     
-def lanes_counter_taz_locations(df):
-    # contador de edges en ruta
-    df['lane_count'] = df['route_edges'].apply(lambda x: len(x.split()))
-    df = df.filter(items=['vehicle_id', 'lane_count', 'vehicle_fromTaz', 'vehicle_toTaz']).rename(columns={'vehicle_id':'ID'})
-    return save_file(df, 'taz_locations_edgenum_df' )
+def SUMO_preprocess(options):
+    # Process SUMO output files to build the dataset
+    def xml2csv(options): 
+        # convert xml to csv
+        files_list = os.listdir(options.sumofiles)
+        if files_list:
+            print('\nGenerating csv files .........')
+            converted_files_dic = {}
+            for f in files_list:
+                # output directory
+                output = os.path.join(options.sumofiles,'..','xmltocsv', f'{f.strip(".xml")}.csv')
+                # SUMO tool xml into csv
+                sumo_tool = os.path.join(tools, 'xml', 'xml2csv.py')
+                # Run sumo tool with sumo output file as input
+                cmd = 'python {} {} -s , -o {}'.format(sumo_tool, os.path.join(options.sumofiles,f), output)
+                os.system(cmd)
+                print(f'{f} -->  {f.strip(".xml")}.csv')
+                converted_files_dic[f.strip('.xml')] = pd.read_csv(output)  
+            return converted_files_dic
+        else:
+            sys.exit(f"Empty or missing output data files: {options.sumofiles}")
 
 
-def get_positions(df, id,min,max):
-    # get initial and end positions based on ini/end time of vehicle id
-    ini_pos =  df.loc[(df['vehicle_id'] == id) & (df['timestep_time'] == min)].iloc[0]
-    end_pos =  df.loc[(df['vehicle_id'] == id) & (df['timestep_time'] == max)].iloc[0]
-    return id, min, max, ini_pos.vehicle_x, ini_pos.vehicle_y, end_pos.vehicle_x, end_pos.vehicle_y 
-    
-   
-def avrg_speed_and_geo_positions(df):
-    # get average speed
-    speed_df = df.groupby(['vehicle_id']).mean().reset_index()
-    speed_df = speed_df.filter(items=['vehicle_id','vehicle_speed']).rename(columns={'vehicle_id':'ID','vehicle_speed':'avrg_speed'})
-    # Prepare df with ini end times of vehicles 
-    df = df.filter(items=['vehicle_id', 'timestep_time','vehicle_x', 'vehicle_y'])
-    df.dropna(subset = ["vehicle_id"], inplace=True)
-    # get initial end times of vechiel
-    time_df = df.groupby(['vehicle_id']).timestep_time.agg(['min','max']).reset_index()
-    # Get positions df
-    positions_list = [get_positions(df,id,min,max) for id,min,max in zip(time_df['vehicle_id'], time_df['min'], time_df['max'])]
-    positions_df = pd.DataFrame(positions_list, columns=['ID', 'ini_time', 'end_time', 'ini_x_pos', 'ini_y_pos','end_x_pos', 'end_y_pos' ])
-    speed_positions = speed_df.concat(positions_df)
-    
-    return save_file(positions_df, 'veh_speed_positions_df')
-    
-
-def remove_outage_points(df):
-    # filter unfinished trips or teleported vehicles
-    df = df.loc[df['tripinfo_timeLoss'] < 200]     # From the histogram analysis we cas see 200s
-    #df = df[(df['tripinfo_duration'] >= 2000) & (df['tripinfo_duration'] <= 3500)]
-    return df
-    
-
-def encode_data(df):
-    # data encoder==string to numberic
-    df = pd.get_dummies(df, columns=['vehicle_fromTaz', 'vehicle_toTaz'])
-    return df
-    
-
-def fill_na_tripduration_value(df):
-     # missing values 
-    if True in pd.array(df.isnull().any()):
-       print ('\nMissing values:\n',df.isnull().any())
-       #filtered_data = filtered_data.dropna()
-       # fill missing values with mean value e.g. tripinfo duration
-       df['tripinfo_duration'] = df['tripinfo_duration'].fillna(df['tripinfo_duration'].mean())
+    def veh_trip_info(df):
+        # filter know features
+        df = df.filter(items=['tripinfo_duration', 
+                              'tripinfo_routeLength', 
+                              'tripinfo_timeLoss', 
+                              'tripinfo_waitingCount', 
+                              'tripinfo_waitingTime', 
+                              'tripinfo_arrivalLane', 
+                              'tripinfo_departLane',
+                              'tripinfo_id']).rename(columns={'tripinfo_id':'ID'})
+        return save_file(df, 'tripinfo_df')
+    																	      
+        
+    def lanes_counter_taz_locations(df):
+        # contador de edges en ruta
+        df['lane_count'] = df['route_edges'].apply(lambda x: len(x.split()))
+        df = df.filter(items=['vehicle_id', 'lane_count', 'vehicle_fromTaz', 'vehicle_toTaz']).rename(columns={'vehicle_id':'ID'})
+        return save_file(df, 'taz_locations_edgenum_df' )
 
 
-def remove_features(df):
-    # remove no relevant features
-    df = df.drop([
-        'ID',
-        'ini_x_pos',
-        'ini_y_pos',
-        'end_x_pos',
-        'end_y_pos',
-        'ini_time',	
-        'end_time',
-        'lane_count',
-    #    'tripinfo_routeLength'	
-    #    'tripinfo_timeLoss'	,
-        'tripinfo_waitingCount'	,
-        'tripinfo_waitingTime'	,
-        'vehicle_fromTaz_HospitalViladecans',	
-        'vehicle_fromTaz_HospitaldeBarcelona',	
-        'vehicle_toTaz_Barcelona'	,
-        'vehicle_toTaz_Sitges'	,
-        'vehicle_toTaz_Terrasa'	,
-        'tripinfo_arrivalLane',	
-        'tripinfo_departLane'
-                    ],axis=1)
-    return df
-    
-
-def add_features(df):
-    # add features (know the problmem)
-    # speed
-    df['Speed'] = df['tripinfo_routeLength'] / df['tripinfo_duration']
-    return df
+    def get_positions(df, id,min,max):
+        # get initial and end positions based on ini/end time of vehicle id
+        ini_pos =  df.loc[(df['vehicle_id'] == id) & (df['timestep_time'] == min)].iloc[0]
+        end_pos =  df.loc[(df['vehicle_id'] == id) & (df['timestep_time'] == max)].iloc[0]
+        return id, min, max, ini_pos.vehicle_x, ini_pos.vehicle_y, end_pos.vehicle_x, end_pos.vehicle_y 
+        
        
+    def avrg_speed_and_geo_positions(df):
+        # get average speed
+        speed_df = df.groupby(['vehicle_id']).mean().reset_index()
+        speed_df = speed_df.filter(items=['vehicle_id','vehicle_speed']).rename(columns={'vehicle_id':'ID','vehicle_speed':'avrg_speed'})
+        # Prepare df with ini end times of vehicles 
+        df = df.filter(items=['vehicle_id', 'timestep_time','vehicle_x', 'vehicle_y'])
+        df.dropna(subset = ["vehicle_id"], inplace=True)
+        # get initial end times of vechiel
+        time_df = df.groupby(['vehicle_id']).timestep_time.agg(['min','max']).reset_index()
+        # Get positions df
+        positions_list = [get_positions(df,id,min,max) for id,min,max in zip(time_df['vehicle_id'], time_df['min'], time_df['max'])]
+        positions_df = pd.DataFrame(positions_list, columns=['ID', 'ini_time', 'end_time', 'ini_x_pos', 'ini_y_pos','end_x_pos', 'end_y_pos'])
+        # Merge speed and positions df
+        speed_and_positions = speed_df.merge(positions_df, on='ID')
+        return save_file(speed_and_positions, 'avrg_speed_positions_df')
+    
+    
+    sumo_dic = xml2csv(options)                                                                   # Get sumo output files into a dictionary of dataframes
+    taz_locations_edgenum_df = lanes_counter_taz_locations(sumo_dic['vehroute'])                  # Count edges on route from vehroute file and get from/to TAZ locations
+    veh_speed_positions_df = avrg_speed_and_geo_positions(sumo_dic['fcd'])                        # Get average speed and initial/end positions (x,y)
+    tripinfo_df = veh_trip_info(sumo_dic['tripinfo']) 
+    
+    # find/convert sumo simulation .rou/taz files
+    #rou, taz = sim_files_search(options.simfiles)
+    
+    # merge dataframes
+    data = taz_locations_edgenum_df.merge(veh_speed_positions_df,on='ID').merge(tripinfo_df,on='ID')
+    print('Parsed --> data.csv')
+    return save_file(data, 'data.csv')
+    
+    
+    
+def preprocess(df):
+    def remove_outage_points(df):
+        # filter unfinished trips or teleported vehicles
+        df = df.loc[df['tripinfo_timeLoss'] < 200]     # From the histogram analysis we cas see 200s
+        #df = df[(df['tripinfo_duration'] >= 2000) & (df['tripinfo_duration'] <= 3500)]
+        return df
+        
+    
+    def encode_data(df):
+        # data encoder  string to numeric
+        df = pd.get_dummies(df, columns=['vehicle_fromTaz', 'vehicle_toTaz'])
+        return df
+        
+    
+    def fill_na_tripduration_value(df):
+         # missing values 
+        if True in pd.array(df.isnull().any()):
+           print ('\nMissing values:\n',df.isnull().any())
+           #filtered_data = filtered_data.dropna()
+           # fill missing values with mean value e.g. tripinfo duration
+           df['tripinfo_duration'] = df['tripinfo_duration'].fillna(df['tripinfo_duration'].mean())
+           return df
+    
+    def remove_features(df):
+        # remove no relevant features
+        df = df.drop([
+            'ID',
+            'ini_x_pos',
+            'ini_y_pos',
+            'end_x_pos',
+            'end_y_pos',
+            'ini_time',	
+            'end_time',
+            'lane_count',
+        #    'tripinfo_routeLength'	
+        #    'tripinfo_timeLoss'	,
+            'tripinfo_waitingCount'	,
+            'tripinfo_waitingTime'	,
+            'vehicle_fromTaz_HospitalViladecans',	
+            'vehicle_fromTaz_HospitaldeBarcelona',	
+            'vehicle_toTaz_Barcelona'	,
+            'vehicle_toTaz_Sitges'	,
+            'vehicle_toTaz_Terrasa'	,
+            'tripinfo_arrivalLane',	
+            'tripinfo_departLane'
+                        ],axis=1)
+        return df
+    
+    def add_features(df):
+        # add features (know the problmem)
+        # speed
+        df['Speed'] = df['tripinfo_routeLength'] / df['tripinfo_duration']
+        return df
+    
+    df = remove_outage_points(df) 
+    df = encode_data(df) 
+    #df = fill_na_tripduration_value(df)
+    df = remove_features(df)                                                                  # remove unused features
+    #df = add_features(df)  
+    return save_file(df,'preprocess_data')       
+
+
+def scale_df(df, v_predicted): 
+    # Scale preprocess values
+    pre_scaled = df.copy()
+    df_scaled = pre_scaled.drop([v_predicted], axis=1)                                    # before scaling we should remove the output
+    df_scaled = scale(df_scaled)
+    # concat scaled and output 
+    col = df.columns.tolist()
+    col.remove(v_predicted)
+    df_scaled = pd.DataFrame(df_scaled, columns=col, index=df.index)
+    df_scaled = pd.concat([df_scaled, df[v_predicted]], axis=1)
+    return save_file(df_scaled,'scaled_data')           
+     
 
 def feature_importance(df, X_train, y_train):
     # Assessing feature importance with random forests
